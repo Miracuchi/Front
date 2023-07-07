91fbe52cc3e0c9676cba1cb02b9274447d838377
Dylan Dorvilma <dda971@live.fr>
2023-07-07 09:09:55 +0200

    V1 searchBar Tictactrip

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './Components/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
